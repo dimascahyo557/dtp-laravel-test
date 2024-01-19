5ed016b9c441824cb86deb1e0308503dc7d4eed8
dimascahyo557 <dimasamid557@gmail.com>
2024-01-19 14:56:56 +0700

    Create: create employees, employee_education, employee_work_experiences table

---
+++ database/migrations/2024_01_19_075012_create_employee_education_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee_education', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->string('school_name', 150);
+            $table->string('major', 150);
+            $table->year('entry_year');
+            $table->year('graduation_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_education');
+    }
+};
